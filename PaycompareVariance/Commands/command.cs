1f3ba33daccffa07c8096953e0d4dcca6f5ed6a7
Pablo B. Martinez Pedroso <pmartinezpedroso@gmail.com>
2025-02-23 22:51:33 -0300

    Add project files.

---
+++ PaycompareVariance/Commands/command.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using PaycompareVariance.Services;
+using Task = System.Threading.Tasks.Task;
+
+namespace PaycompareVariance.Commands
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class command
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("66fe9399-6bf2-4041-b86e-7043d4a38769");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="command"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private command(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            /*ThreadHelper.ThrowIfNotOnUIThread();
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "command";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.package,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);*/
+
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            // Capture variables (name, description, ticket number)
+            string name = "name";
+            string description = "description";
+            string jiraNumber = "Jira number";
+            var service = new ProjectService();
+
+            var validatorPath = service.GetFilePathForValidator(name);
+            var validatorContent = service.GetContentForValidator(name, description);
+            service.WriteClassFile(validatorPath, validatorContent);
+
+            var validatorTestPath = service.GetFilePathForValidatorTest(name);
+            var validatorTestContent = service.GetContentForValidatorTest(name, description);
+            service.WriteClassFile(validatorTestPath, validatorTestContent);
+
+            var sequence = service.GetSecuenceForMigration();
+            var validatorPathMigration = service.GetFilePathForValidatorMigration(sequence, name);
+            var validatorContentMigration = service.GetContentForValidatorMigration(name, description, sequence, string.Empty, name, description, jiraNumber);
+            service.WriteClassFile(validatorPathMigration, validatorContentMigration);
+        }
+
+
+        public static command Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in command's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new command(package, commandService);
+        }
+    }
+}
---
+++ PaycompareVariance/Services/ProjectService.cs
@@ -0,0 +1,345 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PaycompareVariance.Services
+{
+    public class ProjectService
+    {
+        private readonly DTE2 _dte;
+        private const string PAYCOMPARE_PROJECT = "PayCompare.Application.Engine";
+        private const string PAYCOMPARE_TESTS_PROJECT = "PayCompare.UnitTests";
+
+        public ProjectService()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            _dte = (DTE2)Package.GetGlobalService(typeof(DTE));
+        }
+
+        public string GetFilePathForValidator(string name)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var project = GetPayCompareProject();
+
+            var projectPath = Path.GetDirectoryName(project.FullName);
+            var folderPath = Path.Combine(projectPath, "Services", "VarianceValidation", "Validators");
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            var filePath = Path.Combine(folderPath, $"{name}.cs");
+            return filePath;
+        }
+
+        public string GetFilePathForValidatorTest(string name)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var project = GetProjectByName(PAYCOMPARE_TESTS_PROJECT) ?? throw new InvalidOperationException($"The project {PAYCOMPARE_TESTS_PROJECT} was not found in this solution.");
+
+            var projectPath = Path.GetDirectoryName(project.FullName);
+            var folderPath = Path.Combine(projectPath, "Application", "Engine", "Services", "VarianceValidation", "Validators");
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            var filePath = Path.Combine(folderPath, $"{name}.cs");
+            return filePath;
+        }
+
+        public string GetFilePathForValidatorMigration(string sequence, string name)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var project = GetPayCompareProject();
+
+            var projectPath = Path.GetDirectoryName(project.FullName);
+            var folderPath = Path.Combine(projectPath, "Storage", "Migrations");
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            var filePath = Path.Combine(folderPath, $"{sequence}_{name}.cs");
+            return filePath;
+        }
+
+        private Project GetPayCompareProject()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var project = GetProjectByName(PAYCOMPARE_PROJECT);
+            if (project == null)
+            {
+                throw new InvalidOperationException($"The project {PAYCOMPARE_PROJECT} was not found in this solution.");
+            }
+            return project;
+        }
+
+        private Project GetProjectByName(string projectName)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var allProjects = GetAllProjects();
+            return allProjects.FirstOrDefault(p => p.Name.Equals(projectName, StringComparison.OrdinalIgnoreCase));
+        }
+
+        private IEnumerable<Project> GetAllProjects()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var projects = new List<Project>();
+            var solutionProjects = _dte.Solution.Projects;
+            var projectEnumerator = solutionProjects.GetEnumerator();
+
+            while (projectEnumerator.MoveNext())
+            {
+                var project = projectEnumerator.Current as Project;
+                if (project != null)
+                {
+                    projects.Add(project);
+                    projects.AddRange(GetSubProjects(project));
+                }
+            }
+
+            return projects;
+        }
+
+        private IEnumerable<Project> GetSubProjects(Project project)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var subProjects = new List<Project>();
+
+            if (project.ProjectItems != null)
+            {
+                foreach (ProjectItem item in project.ProjectItems)
+                {
+                    if (item.SubProject != null)
+                    {
+                        subProjects.Add(item.SubProject);
+                        subProjects.AddRange(GetSubProjects(item.SubProject));
+                    }
+                }
+            }
+
+            return subProjects;
+        }
+
+        public string GetContentForValidator(string name, string description)
+        {
+            string classContent = $@"
+    using System;
+
+    namespace PayCompare.Application.Engine.Services.VarianceValidation.Validators
+    {{
+        /// <summary>
+        /// {description}
+        /// </summary>
+        public class {name}Validator : IVarianceValidator
+        {{
+            public Domain.Models.Enums.VarianceValidation? VarianceValidation =>
+                Domain.Models.Enums.VarianceValidation.{name};
+
+            public ValidatesField(PayCheckField field)
+            {{
+                throw new NotImplementedException();
+            }}
+
+
+            public VarianceValidationResult Validate(PayCheckVariance variance, PayrollRun payrollRun,
+                (UkgProCheckData Before, UkgProCheckData AfterOriginal, UkgProCheckData After) calcData)
+            {{
+                var afterOriginalHelper = new UkgProCheckDataHelper(calcData.AfterOriginal);
+                var afterHelper = new UkgProCheckDataHelper(calcData.After);
+                
+                //if conditions are met, return VarianceValidationResult.Acknowledge 
+                //meaning that the variance is allowed based on the conditions expected
+                //if the conditions are different from expected and  
+                //this validator can't make a decision on the scenario 
+                //it should return VarianceValidationResult.NoAction
+                //so further validators can assess the variance and make a decision
+                
+                return VarianceValidationResult.NoAction;
+            }}
+
+        }}
+    }}";
+            return classContent;
+        }
+
+        public string GetContentForValidatorTest(string name, string description)
+        {
+            string classContent = $@"
+    using PayCompare.Application.Engine.Services.VarianceValidation.Validators;
+    using PayCompare.Domain.Models.Calculations;
+    using PayCompare.Domain.Models;
+    using PayCompare.Domain.Models.Enums;
+    using System.Collections.Generic;
+    using Xunit;
+
+    namespace PayCompare.UnitTests.Application.Engine.Services.VarianceValidation.Validators
+    {{
+        /// <summary>
+        /// {description}
+        /// </summary>
+        public class {name}ValidatorTest
+        {{
+            [Theory]
+            //Uncomment below and complete the test
+            //[InlineData(PayCheckField., true)]
+            public void ShouldReturnIfValidatesField(PayCheckField field, bool expected)
+            {{
+                var result = new {name}Validator().ValidatesField(field);
+                Assert.Equal(expected, result);
+            }}
+
+            [Theory]
+            //Uncomment below and complete test cases
+            //[InlineData(PayCheckField., 1, VarianceValidationResult.Acknowledge)]
+            public void ShouldValidateVariance(PayCheckField field, decimal varianceAmount, VarianceValidationResult expectedResult)
+            {{
+                var variance = new PayCheckVariance()
+                {{
+                    GenNumber = ""0"",
+                    PayCheckFieldId = (int)field,
+                    Variance = varianceAmount
+                }};
+                //Complete E_Batch, D_Batch, T_Batch with the required properties as needed to test the validator
+                var afterOrigCalcData = new UkgProCheckData()
+                {{
+                    E_Batch = new[] 
+                    {{
+                        new Dictionary<string, object>()
+                        {{
+                            {{ ""EbtGenNumber"", ""0"" }},
+                        }}                    
+                    }},
+                    D_Batch = new[] 
+                    {{ 
+                        new Dictionary<string, object>()
+                        {{
+                            {{ ""DbtGenNumber"", ""0"" }},
+                        }}
+                    }},
+                    T_Batch = new[] 
+                    {{ 
+                        new Dictionary<string, object>()
+                        {{
+                            {{ ""TbtGenNumber"", ""0"" }},
+                        }}
+                    }}
+                }};
+
+                var afterCalcData = new UkgProCheckData()
+                {{
+                    E_Batch = new[] 
+                    {{
+                        new Dictionary<string, object>()
+                        {{
+                            {{ ""EbtGenNumber"", ""0"" }},
+                        }}                    
+                    }},
+                    D_Batch = new[] 
+                    {{ 
+                        new Dictionary<string, object>()
+                        {{
+                            {{ ""DbtGenNumber"", ""0"" }},
+                        }}
+                    }},
+                    T_Batch = new[] 
+                    {{ 
+                        new Dictionary<string, object>()
+                        {{
+                            {{ ""TbtGenNumber"", ""0"" }},
+                        }}
+                    }}
+                }};
+
+                var result = new {name}Validator()
+                    .Validate(variance, new PayrollRun(), (new UkgProCheckData(), afterOrigCalcData, afterCalcData));
+                Assert.Equal(expectedResult, result);
+            }}
+
+        }}
+    }}";
+            return classContent;
+        }
+
+        public string GetContentForValidatorMigration(string name, string description, string migrationSequence, string migrationId, string migrationName, string migrationDesription, string migrationJira)
+        {
+            string classContent = $@"
+    using PayCompare.Infrastructure.Storage.Migrations;
+    using SimpleMigrations;
+
+    namespace PayCompare.Application.Engine.Storage.Migrations
+    {{
+        /// <summary>
+        /// {description}
+        /// </summary>
+        [Migration({migrationSequence}, ""{description}"")]
+        public class {name} : PayCompareMigration
+        {{
+            //TODO: Remember to complete the appropriate migration id for the Variance Validation row 
+            protected override void Up()
+            {{
+                Execute(@"" 
+                    INSERT INTO VarianceValidation(Id, Name, Description, JiraId)
+                    VALUES ({migrationId}, '{migrationName}', '{migrationDesription}', '{migrationJira}');
+                "");
+            }}
+
+            protected override void Down()
+            {{
+            }}
+        }}
+    }}";
+
+            return classContent;
+        }
+
+        public void WriteClassFile(string filePath, string content)
+        {
+            File.WriteAllText(filePath, content);
+        }
+
+        public IEnumerable<string> GetFilesWithPattern(string folderPath, string pattern)
+        {
+            if (!Directory.Exists(folderPath))
+            {
+                throw new DirectoryNotFoundException($"The directory '{folderPath}' does not exist.");
+            }
+
+            var regex = new Regex(pattern);
+            var files = Directory.GetFiles(folderPath, "*.cs");
+            return files.Where(file => regex.IsMatch(Path.GetFileName(file)));
+        }
+
+        public string GetSecuenceForMigration()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var project = GetProjectByName(PAYCOMPARE_PROJECT);
+            if (project == null)
+            {
+                throw new InvalidOperationException("No active project found.");
+            }
+            var projectPath = Path.GetDirectoryName(project.FullName);
+            var folderPath = Path.Combine(projectPath, "Storage", "Migrations");
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            //var files = Directory.GetFiles(folderPath, "*.cs");
+            var files = GetFilesWithPattern(folderPath, @"^\d{4}_");
+            var lastFile = files.LastOrDefault();
+            if (lastFile == null)
+            {
+                return "0010";
+            }
+            var lastFileName = Path.GetFileNameWithoutExtension(lastFile);
+            var sequence = lastFileName.Split('_')[0];
+            return (int.Parse(sequence) + 10).ToString("D4");
+        }
+    }
+}
